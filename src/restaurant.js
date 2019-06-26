432214424d5e2f0c5219896e6e30e4447dfdf088
Reutov Andrey <reutov.andrey@gmail.com>
2019-06-26 22:16:29 +0300

    1.1 + 1.2

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "antd";
+import ReviewList from "./reviews-list";
 
 export default function Restaurant({ restaurant, isOpen, onBtnClick }) {
   const body = isOpen && (
     <>
-      <img src={restaurant.image} width={64} height={64} />
+      <img src={restaurant.image} alt="" width={64} height={64} />
       <div>Menu items: {restaurant.menu.length}</div>
+      <ReviewList reviews={restaurant.reviews} isOpen={isOpen} />
     </>
   );
   return (
