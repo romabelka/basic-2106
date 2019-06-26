5817895d0b3dbaf8270c8037263793d7902d682f
Svetlana Sayganova <ssaiganova@gmail.com>
2019-06-26 23:36:06 +0300

    Логика показа/скрытия списка ревью реализована с помощью хуков

--- src/reviews-list.js
+++ src/reviews-list.js
@@ -1,45 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Review from "./review";
 import { Button, List } from "antd";
 
-export default class ReviewsList extends React.Component {
-  state = {
-    isShown: false
-  };
+export default function ReviewsList({ reviews }) {
+  const [isShown, toggleShown] = useState(false);
 
-  toggleShown = () => {
-    this.setState({ isShown: !this.state.isShown });
-  };
-
-  render() {
-    const { reviews } = this.props;
-    const { isShown } = this.state;
-
-    const body = isShown && (
-      <List bordered>
-        {reviews.map(review => (
-          <Review key={review.id} review={review} />
-        ))}
-      </List>
-    );
-
-    return (
-      <>
-        <Button onClick={this.toggleShown}>
-          {isShown ? "hide reviews" : "show reviews"}
-        </Button>
-        {body}
-      </>
-    );
-  }
+  const body = isShown && (
+    <List bordered>
+      {reviews.map(review => (
+        <Review key={review.id} review={review} />
+      ))}
+    </List>
+  );
+  return (
+    <>
+      <Button onClick={() => toggleShown(!isShown)}>
+        {isShown ? "hide reviews" : "show reviews"}
+      </Button>
+      {body}
+    </>
+  );
 }
-
-// export default function ReviewsList({ reviews }) {
-//   return (
-//     <List bordered>
-//       {reviews.map(review => (
-//         <Review key={review.id} review={review} />
-//       ))}
-//     </List>
-//   );
-// }
