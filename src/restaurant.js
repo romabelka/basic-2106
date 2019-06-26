db97b06d7c14d715dcff8753ed4df28545b783a3
alxmac <alxmac24@gmail.com>
2019-06-26 16:43:17 +0300

    add a list of reviews to an open restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -6,6 +6,13 @@ export default function Restaurant({ restaurant, isOpen, onBtnClick }) {
     <>
       <img src={restaurant.image} width={64} height={64} />
       <div>Menu items: {restaurant.menu.length}</div>
+      <ul>
+        {restaurant.reviews.map(review => (
+          <li key={review.id}>
+            <i>Review</i>: {review.text}; <i>Rating</i>: {review.rating}
+          </li>
+        ))}
+      </ul>
     </>
   );
   return (
