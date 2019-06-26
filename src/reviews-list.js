c52617c06850611f8245c1afc781912ac9f75d47
Svetlana Sayganova <ssaiganova@gmail.com>
2019-06-26 23:23:01 +0300

    Добавлена логика показа/скрытия списка ревью при помощи класса

--- src/reviews-list.js
+++ src/reviews-list.js
@@ -1,13 +1,45 @@
 import React from "react";
 import Review from "./review";
-import { List } from "antd";
+import { Button, List } from "antd";
 
-export default function ReviewsList({ reviews }) {
-  return (
-    <List bordered>
-      {reviews.map(review => (
-        <Review key={review.id} review={review} />
-      ))}
-    </List>
-  );
+export default class ReviewsList extends React.Component {
+  state = {
+    isShown: false
+  };
+
+  toggleShown = () => {
+    this.setState({ isShown: !this.state.isShown });
+  };
+
+  render() {
+    const { reviews } = this.props;
+    const { isShown } = this.state;
+
+    const body = isShown && (
+      <List bordered>
+        {reviews.map(review => (
+          <Review key={review.id} review={review} />
+        ))}
+      </List>
+    );
+
+    return (
+      <>
+        <Button onClick={this.toggleShown}>
+          {isShown ? "hide reviews" : "show reviews"}
+        </Button>
+        {body}
+      </>
+    );
+  }
 }
+
+// export default function ReviewsList({ reviews }) {
+//   return (
+//     <List bordered>
+//       {reviews.map(review => (
+//         <Review key={review.id} review={review} />
+//       ))}
+//     </List>
+//   );
+// }
