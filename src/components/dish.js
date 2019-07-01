c2c862547f35e6ed22c4ade8b8ec907d055157f7
Andrey Reutov <reutov.andrey@gmail.com>
2019-07-01 18:56:39 +0300

    hw2 almost done...

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Button } from "antd";
 import PropTypes from "prop-types";
 
 function Dish(props) {
+  const [amount, setAmount] = useState(0);
+  const onPlusClick = () => setAmount(amount + 1);
+  const onMinusClick = () => (amount ? setAmount(amount - 1) : 0);
   return (
     <Card
       bordered
       actions={[
         `$${props.price}`,
         <>
-          <span style={{ margin: "0 12px" }}>{0}</span>
+          <span data-id="span" style={{ margin: "0 12px" }}>
+            {amount}
+          </span>
           <Button.Group>
-            <Button type="primary" shape="circle" icon="minus" />
-            <Button type="primary" shape="circle" icon="plus" />
+            <Button
+              onClick={onMinusClick}
+              data-id="minus-btn"
+              type="primary"
+              shape="circle"
+              icon="minus"
+            />
+            <Button
+              onClick={onPlusClick}
+              data-id="plus-btn"
+              type="primary"
+              shape="circle"
+              icon="plus"
+            />
           </Button.Group>
         </>
       ]}
--- src/components/review-list.js
+++ src/components/review-list.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Button, List } from "antd";
+import { Button, List, Rate } from "antd";
 import useToggler from "../custom-hooks/use-toggle-open";
 import Review from "./review";
 
 function ReviewList({ reviews }) {
   const { isOpen, toggleOpen } = useToggler();
   const body = isOpen && (
-    <List>
-      {reviews.map(review => (
-        <List.Item key={review.id}>
-          <Review review={review} />
-        </List.Item>
-      ))}
-    </List>
+    <>
+      <List>
+        {reviews.map(review => (
+          <List.Item key={review.id}>
+            <Review review={review} />
+          </List.Item>
+        ))}
+      </List>
+      <Rate />
+    </>
   );
   return (
     <div>
