91ab1284c30bc54b8e944bcda685aee9cb5a89fb
Svetlana Sayganova <ssaiganova@gmail.com>
2019-07-04 00:03:43 +0300

    Добавлена возможность выбора ресторанов по среднему рейтингу

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import { restaurants } from "./fixtures";
 import "antd/dist/antd.css";
 import OrderForm from "./components/order-form";
 import Cart from "./components/cart";
+import RestaurantSelect from "./components/restaurant-select";
 
 export default function App() {
   return (
     <div>
       <h1>Delivery App</h1>
+      <RestaurantSelect />
       <RestaurantsList restaurants={restaurants} />
       <OrderForm />
       <Cart />
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,6 @@ export const DECREMENT = "DECREMENT";
 
 export const ADD_ITEM = "ADD_ITEM";
 export const REMOVE_ITEM = "REMOVE_ITEM";
+
+export const RESTAURANT_RAITNGS = [1, 2, 3, 4, 5];
+export const SELECT_RAITING = "SELECT_RAITING";
