99ffe7ac064ae54a0ac8b19d961a5e4eea1e6666
dnlrbz <danilrabizo97@gmail.com>
2019-07-04 17:27:22 +0200

    HT3

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import { restaurants } from "./fixtures";
 import "antd/dist/antd.css";
 import OrderForm from "./components/order-form";
 import Cart from "./components/cart";
+import RatingSelector from "./components/rating-selector";
 
 export default function App() {
   return (
     <div>
       <h1>Delivery App</h1>
+      <RatingSelector />
       <RestaurantsList restaurants={restaurants} />
       <OrderForm />
       <Cart />
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import counterReducer from "./counter";
 import order from "./order";
+import filter from "./filter";
 
 export default combineReducers({
   count: counterReducer,
-  order
+  order,
+  filter: filter
 });
