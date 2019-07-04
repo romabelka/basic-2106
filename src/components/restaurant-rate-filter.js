dbc0087d983d68abff84e2e5ceae71e2f4abadd9
lazysergey <ohotnik.sergey@gmail.com>
2019-07-04 22:00:04 +0300

    removing comments

--- src/components/restaurant-rate-filter.js
+++ src/components/restaurant-rate-filter.js
@@ -2,37 +2,10 @@ import { connect } from "react-redux";
 import { Select } from "antd";
 import { setMinRating } from "../ac";
 
-// function handleChange(value) {
-//   console.log(`selected ${value}`);
-// }
-
-// ReactDOM.render(
-//   <div>
-//     <Select defaultValue="lucy" style={{ width: 120 }} onChange={handleChange}>
-//       <Option value="jack">Jack</Option>
-//       <Option value="lucy">Lucy</Option>
-//       <Option value="disabled" disabled>
-//         Disabled
-//       </Option>
-//       <Option value="Yiminghe">yiminghe</Option>
-//     </Select>
-//   </div>,
-//   mountNode,
-// );
-
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-// import { Rate } from "antd";
 const { Option } = Select;
 
 class RestaurantRateFilter extends Component {
-  // static propTypes = {
-  //   restaurant: PropTypes.object.isRequired
-  // };
-  // state = {
-  //   rate: getDefaultRate(this.props.restaurant)
-  // };
-
   handleChange = () => {
     console.log("CHANGE O_o");
   };
@@ -53,15 +26,6 @@ class RestaurantRateFilter extends Component {
     );
   }
 }
-
-// function getDefaultRate(restaurant) {
-//   return restaurant.reviews
-//     .map(review => review.rating)
-//     .filter(rate => typeof rate !== "undefined")
-//     .reduce((acc, el, _, arr) => acc + el / arr.length, 0);
-// }
-
-// RestaurantRate.propTypes = {};
 
 const mapStateToProps = (state, ownProps) => ({
   minRating: state.rating
