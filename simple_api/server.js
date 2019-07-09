d8074efbe5ae4d78403917ff774a5c3ed98d3eb2
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-07-09 18:16:09 +0300

    add simple_api

---
+++ simple_api/server.js
@@ -0,0 +1,23 @@
+var path = require("path");
+var express = require("express");
+var api = require("./api");
+var bodyParser = require("body-parser");
+var port = 3001;
+
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(bodyParser.json());
+app.use("/api", api);
+
+app.listen(port, "localhost", function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("Listening at http://localhost:" + port);
+});
