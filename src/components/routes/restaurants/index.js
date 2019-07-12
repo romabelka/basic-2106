df4371e8a80b44f0838b0e6ee34ecadfc647f5f7
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-07-12 19:51:46 +0300

    add children example

--- src/components/routes/restaurants/index.js
+++ src/components/routes/restaurants/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import RestaurantsList from "../../restaurants-list";
 import { Route } from "react-router-dom";
-import RestaurantPage from "./restaurant";
+import Restaurant from "../../restaurant";
 
 export default function RestaurantsPage() {
   return (
     <div>
       <RestaurantsList />
-      <Route path="/restaurants/:id" component={RestaurantPage} />
+      <Route path="/restaurants/:id" children={renderRestaurant} />
     </div>
   );
+}
+
+function renderRestaurant({ match }) {
+  if (!match) return <h1>Please select a restaurant</h1>;
+
+  return <Restaurant id={match.params.id} isOpen />;
 }
