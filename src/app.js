1d98d4657ff292498fc362c9a84816b8168c832b
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-07-12 19:56:50 +0300

    add Switch and exact

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import "antd/dist/antd.css";
 import OrderForm from "./components/order-form";
 import Cart from "./components/cart";
@@ -16,9 +16,15 @@ export default function App() {
       <NavLink to="/restaurants">Restaurants</NavLink>
       <NavLink to="/filter">Filter</NavLink>
 
-      <Route path="/filter" component={Filter} />
-      <Route path="/restaurants" component={RestaurantsPage} />
-      <Route path="/checkout" component={OrderForm} />
+      <Switch>
+        <Route path="/filter" exact component={Filter} />
+        <Route
+          path="/restaurants/:id/review"
+          render={({ id }) => <h1>Add a review for {id}</h1>}
+        />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route path="/checkout" component={OrderForm} />
+      </Switch>
     </div>
   );
 }
