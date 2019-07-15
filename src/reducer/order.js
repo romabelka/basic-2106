0f5a61f3612c069bdbfbbabc9f4beb7a70a02260
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-07-15 20:52:30 +0300

    Add files via upload

--- src/reducer/order.js
+++ src/reducer/order.js
@@ -1,17 +1,19 @@
 import { ADD_ITEM, REMOVE_ITEM } from "../constants";
 import { Map } from "immutable";
 
-export default (order = new Map({}), action) => {
-  const { type, payload } = action;
+const order = new Map({
+})
+
+export default ( state = order, { type, payload }) => {
 
   switch (type) {
     case ADD_ITEM:
-      return order.update(payload.id, 0, amount => amount + 1);
+      return state.update(payload.id, 0, amount => amount + 1);
 
     case REMOVE_ITEM:
-      return order.update(payload.id, 0, amount => Math.max(0, amount - 1));
+      return state.update(payload.id, 0, amount => Math.max(0, amount - 1));
 
     default:
-      return order;
+      return state;
   }
 };
