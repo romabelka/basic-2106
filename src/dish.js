28664ae5503246d666da8fbc8e592705cc3e7d95
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-07-15 20:53:21 +0300

    Add files via upload

---
+++ src/dish.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Card, Button } from "antd";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addItem, removeItem } from "../ac";
+import { dishSelector } from "../selectors";
+
+function Dish({ dish, amount, handleDecrease, handleIncrease }) {
+
+  return (
+    <Card
+      bordered
+      actions={[
+        `$${dish.price}`,
+        <>
+          <span style={{ margin: "0 12px" }} data-id="dish-amount">
+            {amount}
+          </span>
+          <Button.Group>
+            <Button
+              type="primary"
+              shape="circle"
+              icon="minus"
+              data-id="dish-minus"
+              onClick={() => handleDecrease(dish.id)}
+            />
+            <Button
+              type="primary"
+              shape="circle"
+              icon="plus"
+              data-id="dish-plus"
+              onClick={() => handleIncrease(dish)}
+            />
+          </Button.Group>
+        </>
+      ]}
+    >
+      <Card.Meta title={dish.name} description={dish.ingredients.join(", ")} />
+    </Card>
+  );
+}
+
+Dish.defaultProps = {
+  name: "Unknown"
+};
+
+Dish.propTypes = {
+  price: PropTypes.number.isRequired,
+  name: PropTypes.string,
+  ingredients: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  amount: state.order.get(ownProps.id) || 0,
+  dish: dishSelector(state, ownProps)
+});
+
+const mapDispatchToProps = {
+  handleIncrease: addItem,
+  handleDecrease: removeItem
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dish);
