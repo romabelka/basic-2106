a102fa204e30a2eb4fec4ef449eb1f9f912bb654
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-07-15 20:51:13 +0300

    app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, NavLink, Switch } from "react-router-dom";
 import "antd/dist/antd.css";
-import OrderForm from "./components/order-form";
 import Cart from "./components/cart";
 import Filter from "./components/filter";
 import RestaurantsPage from "./components/routes/restaurants";
@@ -9,21 +8,23 @@ import RestaurantsPage from "./components/routes/restaurants";
 export default function App() {
   return (
     <div>
-      <h1>Delivery App</h1>
-      <NavLink to="/checkout">
-        <Cart />
-      </NavLink>
-      <NavLink to="/restaurants">Restaurants</NavLink>
-      <NavLink to="/filter">Filter</NavLink>
 
+      <h1>Delivery App</h1>
+      <NavLink to="/checkout">Checkout</NavLink>
+      <br/>
+      <NavLink to="/restaurants">Restaurants</NavLink>
+      <br/>
+      <NavLink to="/filter">Filter</NavLink>
+      <br/>
+      <br/>
       <Switch>
-        <Route path="/filter" exact component={Filter} />
+        <Route path="/restaurants" component={RestaurantsPage} />
         <Route
           path="/restaurants/:id/review"
           render={({ id }) => <h1>Add a review for {id}</h1>}
         />
-        <Route path="/restaurants" component={RestaurantsPage} />
-        <Route path="/checkout" component={OrderForm} />
+        <Route path='/checkout' exact component={Cart} />
+        <Route path="/filter" exact component={Filter} />
       </Switch>
     </div>
   );
