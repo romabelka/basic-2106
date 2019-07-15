c28279d19c5b496b885d6663654e54525ab80f06
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-07-15 20:53:43 +0300

    Add files via upload

--- src/components/routes/restaurants/index.js
+++ src/components/routes/restaurants/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import RestaurantsList from "../../restaurants-list";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Restaurant from "../../restaurant";
 
 export default function RestaurantsPage() {
   return (
     <div>
-      <RestaurantsList />
-      <Route path="/restaurants/:id" children={renderRestaurant} />
+      <Switch>
+        <Route path="/restaurants/:id" children={renderRestaurant} />
+        <Route path="/restaurants" component={RestaurantsList} />
+      </Switch>
     </div>
   );
 }
 
 function renderRestaurant({ match }) {
-  if (!match) return <h1>Please select a restaurant</h1>;
-
+ 
+  if (!match) 
+    return <h1>Please select a restaurant</h1>;  
   return <Restaurant id={match.params.id} isOpen />;
 }
