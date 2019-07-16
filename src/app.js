99f1be1f516108ec14ff7c1888265d1fe7d812b2
ILIA KNIAZEV <Ilia.Kniazev@noveogroup.com>
2019-07-16 12:24:00 +0300

    added Error on not existing page

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, withRouter } from "react-router-dom";
 import "antd/dist/antd.css";
-import HomePage from "./components/routes/home";
+import Cart from "./components/cart";
+import RestaurantsPage from "./components/routes/restaurants";
+import OrderPage from "./components/routes/order";
+import Error from "./components/routes/error";
 
-export default function App() {
-  return <Route path="/" component={HomePage} />;
+function App() {
+  return (
+    <div>
+      <h1>Delivery App</h1>
+
+      <NavLink to="/checkout">
+        <Cart />
+      </NavLink>
+      <NavLink to="/restaurants">Restaurants List</NavLink>
+
+      <Switch>
+        <Route path="/" exact render={() => null} />
+        <Route path="/restaurants" exact component={RestaurantsPage} />
+        <Route
+          path="/restaurants/:id/review"
+          render={({ id }) => <h1>Add a review for {id}</h1>}
+        />
+        <Route path="/checkout" component={OrderPage} />
+        <Route component={Error} />
+      </Switch>
+    </div>
+  );
 }
+
+export default withRouter(App);
